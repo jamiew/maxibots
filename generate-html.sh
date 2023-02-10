0c53129536f6e55f13bf21b163ad7fba767b6531
Jacob Bijani <jake@users.noreply.github.com>
2023-02-10 11:27:39 -0500

    tweak css

--- generate-html.sh
+++ generate-html.sh
@@ -7,14 +7,15 @@ echo "<style>
 body { background-color: rgb(225, 223, 219); }
 h1 { text-align: center; }
 h1 img#logo { width: 50%; margin: 0 auto; }
+#container {
+  max-width: 800px;
+  margin: 0 auto 50px auto;
+}
 #grid {
   display: grid;
   grid-template-columns: repeat(5, 1fr);
   justify-items: center;
   gap: 3px 3px;
-  /* max-width: 800px; */
-  margin: 0 auto;
-  /* padding: 100px 50px; */
 }
 #grid div img { width: 100%; }
 a { color: #f0f; }
