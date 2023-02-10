70527391113b106e90da24ade94450a9718c94c6
Jamie Dubs <jamie@jamiedubs.com>
2023-02-10 11:09:28 -0500

    This one's for @jake

--- generate-html.sh
+++ generate-html.sh
@@ -3,7 +3,16 @@
 filename="inscriptions.json"
 
 echo "<html><body>"
+echo "<style>
+/* body { display: grid; grid-template-columns: repeat(3, 1fr); grid-gap: 1rem; } */
+body { background-color: rgb(225, 223, 219); }
+div { text-align: center; text-size: 80%; width: 30%; float: left; margin-right: 1rem; margin-bottom: 1rem; }
+div img { width: 100%; }
+a { color: #f0f; }
+</style>"
 
-cat inscriptions.json | jq -r '.[] .inscription' | xargs -n1 sh -c 'echo "<iframe src=\"https://ordinals.com/preview/$1\" width=\"770\" height=\"770\" scrolling=\"no\" style=\"overflow: hidden;\"></iframe><br />"' sh
+cat inscriptions.json |
+  jq -r '.[] .inscription' |
+  xargs -n1 sh -c 'echo "<div><a href=\"https://ordinals.com/inscriptions/$1\"><img src=\"https://ordinals.com/preview/$1\" /></a></div>"' sh
 
 echo "</body</html>"
